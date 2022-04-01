c35351641e71c8397db5bdfab2b579af9abc7372
nickswebsite <send_me_spam@yahoo.com>
2022-04-01 13:32:53 -0400

    Revert "Added jsonpointer to dependencies."
    This reverts commit 9b35f424

--- sphinxcontrib/openapi/schema_utils.py
+++ sphinxcontrib/openapi/schema_utils.py
@@ -1,17 +1,7 @@
 """OpenAPI schema utility functions."""
-import collections
-import copy
+
 from io import StringIO
 
-import deepmerge
-
-# collections.Mapping has been moved to `collections.abc.Mapping` in python 3.10
-from jsonpointer import resolve_pointer
-
-try:
-    Mapping = collections.abc.Mapping
-except AttributeError:
-    Mapping = collections.Mapping
 
 _DEFAULT_EXAMPLES = {
     "string": "string",
@@ -80,17 +70,17 @@ def example_from_schema(schema):
     elif "enum" in schema:
         return schema["enum"][0]
 
-    elif "type" not in schema and not "properties" in schema and "items" not in schema:
+    elif "type" not in schema:
         # Any type
         return _DEFAULT_EXAMPLES["integer"]
 
-    elif "properties" in schema or schema.get("type") == "object":
+    elif schema["type"] == "object" or "properties" in schema:
         example = {}
         for prop, prop_schema in schema.get("properties", {}).items():
             example[prop] = example_from_schema(prop_schema)
         return example
 
-    elif "items" in schema or schema.get("type") == "array":
+    elif schema["type"] == "array":
         items = schema["items"]
         min_length = schema.get("minItems", 0)
         max_length = schema.get("maxItems", max(min_length, 2))
@@ -145,141 +135,3 @@ def example_from_schema(schema):
 
     else:
         return _DEFAULT_EXAMPLES[schema["type"]]
-
-
-_merge_mappings = deepmerge.Merger(
-    [(Mapping, deepmerge.strategy.dict.DictStrategies("merge"))],
-    ["override"],
-    ["override"],
-).merge
-
-
-def _get_schema_type(schema):
-    """Retrieve schema type either by reading 'type' or guessing."""
-
-    # There are a lot of OpenAPI specs out there that may lack 'type' property
-    # in their schemas. I fount no explanations on what is expected behaviour
-    # in this case neither in OpenAPI nor in JSON Schema specifications. Thus
-    # let's assume what everyone assumes, and try to guess schema type at least
-    # for two most popular types: 'object' and 'array'.
-    if "type" not in schema:
-        if "properties" in schema:
-            schema_type = "object"
-        elif "items" in schema:
-            schema_type = "array"
-        else:
-            schema_type = None
-    else:
-        schema_type = schema["type"]
-    return schema_type
-
-
-def traverse_schema(top_level, block, name, is_required=False):
-    schema_type = _get_schema_type(block)
-    if "$ref" in block:
-        yield from traverse_schema(
-            top_level,
-            resolve_reference(top_level, block["$ref"]),
-            name,
-            is_required,
-        )
-    elif {"oneOf", "anyOf", "allOf"} & block.keys():
-        # Since an item can represented by either or any schema from
-        # the array of schema in case of `oneOf` and `anyOf`
-        # respectively, the best we can do for them is to render the
-        # first found variant. In other words, we are going to traverse
-        # only a single schema variant and leave the rest out. This is
-        # by design and it was decided so in order to keep produced
-        # description clear and simple.
-        yield from traverse_schema(
-            top_level, resolve_combining_schema(block), name
-        )
-    elif "not" in block:
-        yield name, {}, is_required
-    elif schema_type == "object":
-        if name:
-            yield name, block, is_required
-
-        required = set(block.get("required", []))
-
-        for key, value in block.get("properties", {}).items():
-            # In case of the first recursion call, when 'name' is an
-            # empty string, we should go with 'key' only in order to
-            # avoid leading dot at the beginning.
-            yield from traverse_schema(
-                top_level,
-                value,
-                f"{name}.{key}" if name else key,
-                is_required=key in required,
-            )
-    elif schema_type == "array":
-        yield from traverse_schema(top_level, block["items"], f"{name}[]")
-
-    elif "enum" in block:
-        yield name, block, is_required
-
-    elif schema_type is not None:
-        yield name, block, is_required
-
-
-def resolve_combining_schema(schema):
-    if "oneOf" in schema:
-        # The part with merging is a vague one since I only found a
-        # single 'oneOf' example where such merging was assumed, and no
-        # explanations in the spec itself.
-        merged_schema = schema.copy()
-        merged_schema.update(merged_schema.pop("oneOf")[0])
-        return merged_schema
-
-    elif "anyOf" in schema:
-        # The part with merging is a vague one since I only found a
-        # single 'oneOf' example where such merging was assumed, and no
-        # explanations in the spec itself.
-        merged_schema = schema.copy()
-        merged_schema.update(merged_schema.pop("anyOf")[0])
-        return merged_schema
-
-    elif "allOf" in schema:
-        # Since the item is represented by all schemas from the array,
-        # the best we can do is to render them all at once
-        # sequentially. Please note, the only way the end result will
-        # ever make sense is when all schemas from the array are of
-        # object type.
-        merged_schema = schema.copy()
-        for item in merged_schema.pop("allOf"):
-            merged_schema = _merge_mappings(merged_schema, copy.deepcopy(item))
-        return merged_schema
-
-    elif "not" in schema:
-        # Eh.. do nothing because I have no idea what can we do.
-        return {}
-
-    return schema
-
-
-def rebuild_references(top_level, block):
-    schema_type = _get_schema_type(block)
-    if schema_type == "object":
-        if "properties" in block:
-            properties = {}
-            for k, v in block["properties"].items():
-                properties[k] = rebuild_references(top_level, v)
-            return {**block, "properties": properties}
-    elif schema_type == "array":
-        return {
-            **block,
-            "items": rebuild_references(top_level, block["items"])
-        }
-    elif "$ref" in block:
-        return rebuild_references(
-            top_level, resolve_reference(top_level, block["$ref"])
-        )
-    else:
-        return block
-
-
-def resolve_reference(schema, link):
-    if link.startswith("#"):
-        return resolve_pointer(schema, link[1:]).copy()
-    else:
-        raise NotImplementedError("Resolving references to URIs is not currently supported.")
